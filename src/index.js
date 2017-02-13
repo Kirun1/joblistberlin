7a406131eea4ce4053841b253aa70de3fcf4d4fb
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-02-13 15:16:42 +0100

    component composition + styles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import './main.css';
+
+function Title(props) {
+    return <h1 style={ {color: 'green'} } className="Title">{props.title}</h1>
+}
+
+function App(props) {
+    return (
+	    <Title title={ props.post.title}/>
+    );
+}
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+	<App post={ {title: 'My titleys'} } />,
+    document.getElementById('root')
 );
