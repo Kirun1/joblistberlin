36dd5261b600f5c9ac1c8e1079991d3e34e86958
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-02-13 23:26:37 +0100

    switch from falxa to hugo syntax

--- src/App.js
+++ src/App.js
@@ -4,29 +4,21 @@ import PostJob from './PostJob';
 import {callAPI} from './api';
 
 export default class App extends React.Component {
-
-    componentDidMount()
-    {
-	this.fetchData().then(data => this.setState( { data }));
-    }
-    
-    fetchData()
-    {
-	return callAPI('/links');
-
-    }
-    render()
-    {
-	if(!this.state) {
+  componentDidMount() {
+		this.fetchData().then(data => this.setState( { data }));
+  }
+  fetchData() {
+		return callAPI('/links');
+  }
+  render() {
+		if(!this.state) {
 	    return null;
-	}
-	
-	return (
-		<div>
-		<PostJob/>
-		<Jobs data={ this.state.data }/>
-		</div>
-	);
-    }
+		}
+		return (
+				<div>
+				<PostJob/>
+				<Jobs data={ this.state.data }/>
+				</div>
+		);
+  }
 }
-
