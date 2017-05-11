6ca7e2e8862a6595bf749292296237be70824b2d
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-05-11 14:24:14 +0200

    remove duplicate Router add location

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import { Link,
-				 Route,
-				 BrowserRouter as Router} from 'react-router-dom';
+// BrowserRouter as Router is in <Root/>
+import { Link, Route } from 'react-router-dom';
 import Companies from './Companies';
 import Jobs from './Jobs';
 
 
 export default function App() {
 	return (
-		<Router>
-			<div className="App">
-				<nav className="Nav">
-					<Link to="/companies">Companies</Link>
-					<Link to="/jobs">Jobs</Link>
-				</nav>
+		<div className="App">
+			<nav className="Nav">
+				<Link to="/companies">Companies</Link>
+				<Link to="/jobs">Jobs</Link>
+			</nav>
 
-				<Route path="/companies" component={ Companies }/>
-				<Route path="/jobs" component={ Jobs }/>
-			</div>
-		</Router>
+			<Route path="/companies" component={ Companies }/>
+			<Route path="/jobs" component={ Jobs }/>
+		</div>
 	)
 }
