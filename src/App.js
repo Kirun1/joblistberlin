355cbca6b32c958f322e5e272786f53178f5c78f
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-05-11 13:57:28 +0200

    fix routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import Companies from './Companies';
 import {Link, Route} from 'react-router-dom';
+import Companies from './Companies';
+import Jobs from './Jobs';
+
 
 export default function App() {
 	return (
 		<div>
 			<nav className="Navigation">
-				<Link to="c">Companies</Link>
-				<Link to="j">Jobs</Link>
+				<Link to="/companies">Companies</Link>
+				<Link to="/jobs">Jobs</Link>
 			</nav>
-			<Route path="/c" component={Companies}/>
+			<Route path="/companies" component={ Companies }/>
+			<Route path="/jobs" component={ Jobs }/>
 		</div>
 	)
 }
