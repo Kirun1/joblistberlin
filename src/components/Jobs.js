77a0c8c3511014b397b0fbaf4a0b8821a158e39a
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-05-13 11:25:48 +0200

    add text on jobs page

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -4,7 +4,9 @@ import PostJob from './PostJob.js';
 export default function Jobs() {
 	return (
 		<div className="Jobs">
+			<p>Maybe you need to hire someone fast, or you don't have a website to post a job offer to. Use this form to post a job offer to this site</p>
 			<PostJob/>
+			<p>Job offers:</p>
 		</div>
 	)
 }
