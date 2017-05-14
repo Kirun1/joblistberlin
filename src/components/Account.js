ac3f55e766531c22e31f86ada89e960b83ba3d5c
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-05-14 08:54:08 +0200

    show email in Account

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { getCurrentUser } from '../api';
 
-export default function Reat() {
-	return <p>Settings</p>
+export default class Account extends Component {
+	constructor() {
+		super();
+		this.state = {
+			user: null,
+			email: null
+		}
+	}
+	componentDidMount() {
+		const user = getCurrentUser();
+		this.setState({
+			user,
+			email: user.email
+		})
+	}
+	render() {
+		return (
+			<div>
+				<h2>Account:</h2>
+				<p>email: { this.state.email }</p>
+			</div>
+		)
+	}
 }
