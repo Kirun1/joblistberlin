850451094eca14578c047e100ebb935deac31074
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-05-16 15:45:57 +0200

    display email verificatio nstatus on account

--- src/components/Account.js
+++ src/components/Account.js
@@ -8,22 +8,33 @@ export default class Account extends Component {
 			user: null
 		}
 	}
+
 	componentDidMount() {
 		const user = getCurrentUser();
 		this.setState({
 			user
 		})
 	}
+
+	isEmailVerified() {
+		const { email, emailVerified} = this.state.user;
+		if (emailVerified) {
+			return `${email} (verified)`;
+		} else {
+			return `${email} (not-verified)`;
+		}
+	}
+
 	render() {
 		if(!this.state.user) return null;
 
-		const { email, emailVerified } = this.state.user;
 		return (
 			<div>
 				<h2>Account <small>overview</small></h2>
 				<label>
-					Your email: {emailVerified ? <span>verified</span> : <span>(verify)</span>}
-					<input placeholder={ email }/>
+					Your email:
+					<input disabled={true} placeholder={ this.isEmailVerified() }/>
+					{ !this.state.user.emailVerified && <button onClick={ this.sendVerificationEmail }>Re-send verification email</button> }
 				</label>
 			</div>
 		)
