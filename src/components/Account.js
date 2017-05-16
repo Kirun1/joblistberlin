21689360099043b96d84a79881129cc2bd664f40
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-05-16 15:33:22 +0200

    settings

--- src/components/Account.js
+++ src/components/Account.js
@@ -5,22 +5,26 @@ export default class Account extends Component {
 	constructor() {
 		super();
 		this.state = {
-			user: null,
-			email: null
+			user: null
 		}
 	}
 	componentDidMount() {
 		const user = getCurrentUser();
 		this.setState({
-			user,
-			email: user.email
+			user
 		})
 	}
 	render() {
+		if(!this.state.user) return null;
+
+		const { email, emailVerified } = this.state.user;
 		return (
 			<div>
-				<h2>Account:</h2>
-				<p>email: { this.state.email }</p>
+				<h2>Account <small>overview</small></h2>
+				<label>
+					Your email: {emailVerified ? <span>verified</span> : <span>(verify)</span>}
+					<input placeholder={ email }/>
+				</label>
 			</div>
 		)
 	}
