dab7830f04b485fd169b6daafd14ddd256f7f2f8
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-05-18 09:14:31 +0200

    company addproved or not in css class

--- src/components/Company.js
+++ src/components/Company.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
 export default function Job(props) {
+	let companyStatus;
+
+	if (props.isApproved) {
+		companyStatus = 'Company--isApproved';
+	} else {
+		companyStatus = 'Company--isNotApproved';
+	}
+
   return (
-	  <article key={ props.id } className="Company">
-			<span>{ props.isApproved ? ">" : "<" }</span>
-	    <a href={props.url} className="Company-title">{props.title}</a>
+	  <article key={ props.id } className={`Company ${companyStatus}`}>
+			<span></span>
+	    <h4 className="Company-title">{props.title}</h4>
+			<a href={props.url} className="Company-link">{props.url}</a>
 		</article>
   )
 }
