2ab8898e388ae3e2a8cc0af5c18d7ba6b2b05b3f
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-05-24 18:50:47 +0200

    refactor css naming

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import Root from './Root';
 import './styles/reset.css';
 import './styles/index.css';
 import './styles/layout.css';
-import './styles/buttons.css';
+import './styles/button.css';
+import './styles/form.css';
 import './styles/nav.css';
 import './styles/contextualToggle.css';
-import './styles/notifications.css';
-import './styles/companies.css';
+import './styles/notification.css';
+import './styles/company.css';
 
 ReactDOM.render(
 	<Root />,
