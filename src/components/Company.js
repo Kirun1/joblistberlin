f29e1a7ac4d320c86659f9b492a284100db66c61
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-05-24 23:16:08 +0200

    add temporary company detail view based on createdAt

--- src/components/Company.js
+++ src/components/Company.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import CompanyCard from './CompanyCard.js'
+import React, { Component } from 'react';
+import { database as db } from 'firebase';
+import CompanyCard from './CompanyCard';
+import Loading from './Loading';
 
-export default function Company() {
-	return (
-		<CompanyCard/>
-	)
+export default class Company extends Component {
+	componentDidMount() {
+		const id = Number(this.props.match.params.id);
+		db().ref('/links').orderByChild('createdAt').equalTo(id).on('value', (snapshot) => {
+			const model = snapshot.val();
+			const item = Object.keys(model)
+			this.setState({
+				model: model[item]
+			})
+		})
+	}
+	render() {
+		if(!this.state) return <Loading/>
+		return (
+			<CompanyCard { ...this.state.model }/>
+		)
+	}
 }
