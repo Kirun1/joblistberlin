fb7b2f56bd7ee377d48f10962a5ee4f1ad076609
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2017-05-29 12:00:28 +0200

    toggle favorite on card

--- src/components/CompanyCard.js
+++ src/components/CompanyCard.js
@@ -6,16 +6,27 @@ import ContextualToggle from './ContextualToggle'
 import Option from './Option';
 
 class CompanyCard extends Component {
-	addToUserFavorites = () => {
+	toggleToUserFavorites = () => {
 		const { addNotification,
 						id,
-						title } = this.props;
+						title,
+						isFavorite } = this.props;
+		var status,
+				message;
+
+		if (isFavorite) {
+			status = null;
+			message = `Removed ${title}`;
+		} else {
+			status = true;
+			message = `Added ${title}`;
+		}
 
 		getCurrentUserSettingRef().then(settings => {
 			const settingsId = Object.keys(settings.val())[0];
 			database().ref(`userSettings/${settingsId}/favoriteCompanies/${id}`)
-								.set(true).then(() => {
-									addNotification(`<${title}> added to your favorites`);
+								.set(status).then(() => {
+									addNotification(message);
 								})
 		})
 	}
@@ -34,13 +45,19 @@ class CompanyCard extends Component {
 		const { id,
 						title,
 						url,
-						goToDetail } = this.props;
+						goToDetail,
+						isFavorite } = this.props;
 		return (
 			<article className={`Company ${this.companyIsApproved()}`}>
 
 				<ContextualToggle label={ title }>
 					{/* { goToDetail ? <Option action={ () => goToDetail(id) }>Edit</Option> : null } */}
-					<Option action={ this.addToUserFavorites }>Add to favorites</Option>
+					{ isFavorite ? (
+					<Option action={ this.toggleToUserFavorites }>Remove from favorites</Option>
+					) : (
+					<Option action={ this.toggleToUserFavorites }>Add to favorites</Option>
+					) }
+
 					<Option action={ this.reportBrokenLink }>Report broken link</Option>
 				</ContextualToggle>
 
