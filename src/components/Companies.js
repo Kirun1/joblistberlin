6b8269f35cd29397b308e167e76e936ebfec9939
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2018-01-27 13:53:45 +0100

    add not working search

--- src/components/Companies.js
+++ src/components/Companies.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Jets from 'jets';
 import withFirebase from '../withFirebase';
 import Loading from './Loading';
 import CompanyCard from './CompanyCard';
@@ -7,10 +8,38 @@ import CompanyCard from './CompanyCard';
 class Companies extends Component {
 	constructor() {
 		super();
-		this.state = {}
+		this.state = {
+			jetsSearch: '',
+			jets: {}
+		}
+	}
+
+	componentDidMount() {
+		const jets = new Jets({
+			callSearchManually: true,
+			searchTag: '#jetsSearch',
+			contentTag: '#jetsContent'
+		});
+
+		this.state = {
+			jets
+		}
+	}
+
+	componentWillUnmount() {
+		this.state.jets.destroy();
+	}
+
+	handleSearch = (e) => {
+		this.setState({
+	    [e.target.name] : e.target.value
+		});
+		this.state.jets.search(e.target.value)
 	}
 
 	render() {
+		const { jetsSearch } = this.state;
+
 		if(!this.props.data) return <Loading/>
 
 		return (
@@ -18,13 +47,25 @@ class Companies extends Component {
 				<h2><small>A list of </small>companies</h2>
 				<p>
 					Companies in this list have <strong>offices in Berlin</strong>, and <strong>job offers</strong> on their website.<br/>
-					This is a <strong>community currated</strong> list, anyone can <Link to='companies/add'>submit a company</Link> for review.
+					This is a <strong>community curated</strong> list, anyone can <Link to='companies/add'>submit a company</Link> for review.
 				</p>
 
-				<p>Most recently added:</p>
-				{ this.props.data.map((company, index) => <CompanyCard key={ index }
-																														goToDetail={ this.goToCompanyDetail }
-																														{ ...company } />) }
+				<label>
+					<input
+					type="search"
+					name="jetsSearch"
+					id="jetsSearch"
+					title="Search for a company"
+					placeholder="Search for a company"
+					onChange={ this.handleSearch }
+					value={ jetsSearch } />
+				</label>
+
+				<div id="jetsContent">
+					{ this.props.data.map((company, index) => <CompanyCard key={ index }
+					goToDetail={ this.goToCompanyDetail }
+					{ ...company } />) }
+				</div>
 			</div>
 		)
 	}
