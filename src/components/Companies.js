80a1fa2e2d05a85e5655bfe589e74ab99fdc177b
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2018-01-27 17:07:13 +0100

    replace jet by vanilla js, ty

--- src/components/Companies.js
+++ src/components/Companies.js
@@ -10,37 +10,21 @@ class Companies extends Component {
 		super();
 
 		this.state = {
-			jetsSearch: '',
-			jets: {}
+			search: ''
 		}
 	}
 
-	componentDidMount() {
-		const jets = new Jets({
-			callSearchManually: true,
-			contentTag: '#jetsContent'
-		});
-
-		this.setState({
-			jets
-		})
-	}
-
-	componentWillUnmount() {
-		this.state.jets.destroy();
-	}
-
-
 	handleSearch = (e) => {
 		this.setState({
-	    [e.target.name] : e.target.value
+	    search : e.target.value
 		});
-		this.state.jets.search(e.target.value)
+	}
+
+	applySearch = (company) => {
+		return company.title.toLowerCase().includes(this.state.search.toLowerCase())
 	}
 
 	render() {
-		const { jetsSearch } = this.state;
-
 		if(!this.props.data) return <Loading/>
 
 		return (
@@ -48,24 +32,30 @@ class Companies extends Component {
 				<h2><small>A list of </small>companies</h2>
 				<p>
 					Companies in this list have <strong>offices in Berlin</strong>, and <strong>job offers</strong> on their website.<br/>
-					This is a <strong>community curated</strong> list, anyone can <Link to='companies/add'>submit a company</Link> for review.
-				</p>
+			This is a <strong>community curated</strong> list, anyone can <Link to='companies/add'>submit a company</Link> for review.
+			</p>
 
-				<label>
-					<input
-					type="search"
-					name="jetsSearch"
-					id="jetsSearch"
-					title="Search for a company"
-					placeholder="Search for a company"
-					onChange={ this.handleSearch }
-					value={ jetsSearch } />
+			<label>
+			<input
+			type="search"
+			title="Search for a company"
+			placeholder="Search for a company"
+			onChange={ this.handleSearch }
+			value={ this.state.search } />
 				</label>
 
 				<div id="jetsContent">
-					{ this.props.data.map((company, index) => <CompanyCard key={ index }
+			{
+				this.props.data
+						.filter(this.applySearch)
+						.map((company, index) => (
+					<CompanyCard
+					key={ company.id }
 					goToDetail={ this.goToCompanyDetail }
-					{ ...company } />) }
+					{ ...company }
+					/>
+				))
+			}
 				</div>
 			</div>
 		)
