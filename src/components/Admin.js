22fc44414c23aa08133fae7636440337cdfb4d61
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2018-02-01 14:42:31 +0100

    add google trend map

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -25,6 +25,12 @@ export default class Admin extends Component {
 					<li><a href="https://joblistberlin-staging.firebaseio.com/links/.json">link.json (staging)</a></li>
 					<li><a href="https://trends.google.com/trends/explore?q=job%20list%20berlin,berlin%20startup%20jobs,jobs%20in%20berlin,work%20in%20berlin,berlin%20job">Google trends</a></li>
 				</ul>
+
+				<script type="text/javascript" src="https://ssl.gstatic.com/trends_nrtr/1294_RC01/embed_loader.js"></script>
+				<script type="text/javascript">
+					trends.embed.renderExploreWidget("GEO_MAP", {"comparisonItem":[{"keyword":"job list berlin","geo":"","time":"today 12-m"},{"keyword":"berlin startup jobs","geo":"","time":"today 12-m"},{"keyword":"jobs in berlin","geo":"","time":"today 12-m"},{"keyword":"work in berlin","geo":"","time":"today 12-m"},{"keyword":"berlin job","geo":"","time":"today 12-m"}],"category":0,"property":""}, {"exploreQuery":"q=job%20list%20berlin,berlin%20startup%20jobs,jobs%20in%20berlin,work%20in%20berlin,berlin%20job&date=today 12-m,today 12-m,today 12-m,today 12-m,today 12-m","guestPath":"https://trends.google.com:443/trends/embed/"});
+				</script>
+
 			</div>
 		)
 	}
