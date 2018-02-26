aedef126767954e7fadca9b1224eaec5faeea20c
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2018-02-26 12:02:21 +0100

    add slug, but cna't put has key does not exist in db

--- src/withCompanies.js
+++ src/withCompanies.js
@@ -1,16 +1,26 @@
 import withFirebase from './withFirebase';
 
+function makeSlug(string) {
+	return string.trim() // remove whitespaces on the edge
+							 .replace(/[`~!@#$%^&*()_|+=?;:'",.<>{}[\]\\/]/gi, '') // remove unwanted characters
+							 .replace(/\s+/g, '-') // change spaces groups to single `-`
+							 .replace(/\-+/g, '-') // change - groups to simgle `-`
+							 .toLowerCase()
+}
+
 function serializeCompanies(data, id) {
 	const companyModel = {
 		id: '',
 		body: '',
 		url: '',
 		title: '',
+		/* slug: '',*/
 		createdAt: 0,
 		isApproved: false
 	};
 
 	data.id = id;
+	/* data.slug = makeSlug(data.title)*/
 	return Object.assign(companyModel, data);
 };
 
