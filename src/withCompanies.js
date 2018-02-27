b76f851080457ef83cabb4e36f9458d80fd8810b
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2018-02-27 13:29:40 +0100

    add serializer

--- src/withCompanies.js
+++ src/withCompanies.js
@@ -1,27 +1,4 @@
 import withFirebase from './withFirebase';
+import serializeCompany from './utils/serializers/company';
 
-function makeSlug(string) {
-	return string.trim() // remove whitespaces on the edge
-							 .replace(/[`~!@#$%^&*()_|+=?;:'",.<>{}[\]\\/]/gi, '') // remove unwanted characters
-							 .replace(/\s+/g, '-') // change spaces groups to single `-`
-							 .replace(/\-+/g, '-') // change - groups to simgle `-`
-							 .toLowerCase()
-}
-
-function serializeCompanies(data, id) {
-	const companyModel = {
-		id: '',
-		body: '',
-		url: '',
-		title: '',
-		/* slug: '',*/
-		createdAt: 0,
-		isApproved: false
-	};
-
-	data.id = id;
-	/* data.slug = makeSlug(data.title)*/
-	return Object.assign(companyModel, data);
-};
-
-export default withFirebase('links', serializeCompanies);
+export default withFirebase('links', serializeCompany);
